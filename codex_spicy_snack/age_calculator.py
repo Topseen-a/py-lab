fdd8c6ce50ae9b9dbbbeabdad4b604d45083652b
Topseen-a <abodunrintopseen@gmail.com>
2025-11-13 22:49:08 +0100

    second commit

--- codex_spicy_snack/age_calculator.py
+++ codex_spicy_snack/age_calculator.py
@@ -4,7 +4,7 @@
 #Prompt user to "Enter current son's age from 1 - 80:"
 #Read input of son_age
 
-#Calculate years_difference = Absolute value of (father_age - 2 * son_age)
+#Calculate the years_difference = Absolute value of (father_age - 2 * son_age)
 
 #Display "The father was twice as old as his son", years_difference, "years ago"
 
