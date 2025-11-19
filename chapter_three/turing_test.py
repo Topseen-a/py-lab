1b1dd1a00c29fc6e56e847c059a8252a40099dff
Topseen-a <abodunrintopseen@gmail.com>
2025-11-19 23:15:29 +0100

    chapter three

---
+++ chapter_three/turing_test.py
@@ -0,0 +1,12 @@
+computer = input('What is your problem? ')
+
+response = input('Have you had this problem before? (yes/no): ').lower()
+
+if response == "yes":
+    print('Well you have it again')
+elif response != "yes":
+    print('Well you have it now')
+
+
+
+
