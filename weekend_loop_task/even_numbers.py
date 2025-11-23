565656d576174b1519a1e95f7baf37a90caf5abd
Topseen-a <abodunrintopseen@gmail.com>
2025-11-23 10:52:24 +0100

    weekend fun

--- weekend_loop_task/even_numbers.py
+++ weekend_loop_task/even_numbers.py
@@ -1,7 +1,7 @@
 #for count in range 1-101
 #if count modulo 2 equals 0
 #display count on a straght line
-#duse an epmty print to go to the next line
+#use an epmty print to go to the next line
 
 for count in range(1, 101):
     if count % 2 == 0:
--- weekend_loop_task/multiples_of_three.py
+++ weekend_loop_task/multiples_of_three.py
@@ -1,3 +1,7 @@
+#for count in range 1-16
+#display the result of count as a multiple of 3 on a straight line
+#use an empty print to go to the next line
+
 for count in range(1,16):
     print(count * 3, end='') 
 print()
