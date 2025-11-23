a6562a19a776b292fa357f6fd128bc9d691c0854
Topseen-a <abodunrintopseen@gmail.com>
2025-11-23 10:44:10 +0100

    weekend task

--- weekend_loop_task/even_numbers.py
+++ weekend_loop_task/even_numbers.py
@@ -1,3 +1,8 @@
+#for count in range 1-101
+#if count modulo 2 equals 0
+#display count on a straght line
+#duse an epmty print to go to the next line
+
 for count in range(1, 101):
     if count % 2 == 0:
         print(count, end='')
