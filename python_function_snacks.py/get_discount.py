f7bdaf62f39e6e37b52413f4cdda0bf4916969c5
Topseen-a <abodunrintopseen@gmail.com>
2025-11-26 21:58:48 +0100

    function snacks

--- python_function_snacks.py/get_discount.py
+++ python_function_snacks.py/get_discount.py
@@ -1,1 +1,10 @@
+def get_discount(item_name, original_price, promo_code):
+    if promo_code == 'SAVE10':
+        discount = 0.10
+    elif promo_code == 'HALFOFF':
+        discount = 0.50
+    else:
+        discount = 0
 
+    discounted_price = original price * (1 - discount)
+    return discounted_price
