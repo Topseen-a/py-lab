90f5e03ef8c3693a2b368842e4e547f1070afc50
Topseen-a <abodunrintopseen@gmail.com>
2025-12-27 23:11:59 +0100

    list snack

--- holiday/list_kata.py
+++ holiday/list_kata.py
@@ -6,4 +6,30 @@ def maximum_in(array_list):
         if array_list[count] > maximum:
             maximum = array_list[count]
     return maximum
-print(maximum_in(array_list))
+
+def minimum_in(array_list):
+    minimum = array_list[0]
+    for count in range(0,len(array_list)):
+        if array_list[count] < minimum:
+            minimum = array_list[count]
+    return minimum
+
+def sum_of(array_list):
+    total = 0
+    for count in range(0,len(array_list)):
+        total += array_list[count]
+    return total
+
+def sum_of_even_numbers_in(array_list):
+    total = 0
+    for count in range(0,len(array_list)):
+        if array_list[count] % 2 == 0:
+            total += array_list[count]
+    return total
+
+def sum_of_odd_numbers_in(array_list):
+    total = 0
+    for count in range(0,len(array_list)):
+        if array_list[count] % 2 != 0:
+            total += array_list[count]
+    return total
