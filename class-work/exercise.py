b5603f257c6432840c69a9185779ec6ccf43096c
Topseen-a <abodunrintopseen@gmail.com>
2026-01-27 17:15:17 +0100

    task

--- class-work/exercise.py
+++ class-work/exercise.py
@@ -49,3 +49,35 @@ def enumerate_words(list_of_words):
     return new_list
 
 print(enumerate_words(list_of_words))
+
+# List comprehension
+names = ["Odili", "Fola", "Christian", "Samuel", "Ugo", "Pensiom"]
+name_list = [name for name in names if len(name) > 5]
+
+print(name_list)
+
+# Tuple
+student_tuple = (5,2,3,4,5,434,5,54,4)
+print(student_tuple)
+
+length = len(student_tuple)
+print(length)
+
+numbers = [1,2,3,4,5,]
+numbers += (6,7)
+print(numbers)
+
+number = (["title"], 9)
+number[0].append("poo")
+number[0].append("ink")
+number[0].append("king")
+print(number)
+
+# Filter, Map and Reduce
+# filter (function, iterable)
+numbers = [10,3,7,1,9,4,2,8,5,6]
+def is_odd(number):
+    return number % 2 != 0
+# result = [filter(is_odd, numbers)]
+result = list(filter(is_odd, numbers))
+print(result)
