fde91c2d4a8690a236a60cd131780a331a9a7db8
Topseen-a <abodunrintopseen@gmail.com>
2026-01-29 06:38:13 +0100

    code party

--- assignment/code_party.py
+++ assignment/code_party.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 first_tuple = (1, 2, 3)
 number_to_be_added = 4
 new_tuple = first_tuple + (number_to_be_added,)
@@ -65,3 +67,67 @@ def square(number):
     return number ** 2
 squared_numbers = map(square, range(1, 11))
 print(list(squared_numbers))
+
+names = ["john", "mary", "steve"]
+def capitalize(names):
+    return names.capitalize()
+capitalized_names = map(capitalize, names)
+print(list(capitalized_names))
+
+prices = [100, 200, 300]
+def add_tax(price):
+    return price * 1.10
+prices_with_tax = map(add_tax, prices)
+print(list(prices_with_tax))
+
+
+
+def add(number_one, number_two):
+    return number_one + number_two
+sum_of_numbers = reduce(add, range(1, 51))
+print(sum_of_numbers)
+
+numbers = [3,5,9,2,8]
+def maximum_of_numbers(number_one, number_two):
+    if number_one > number_two:
+        return number_one
+    else:
+        return number_two
+max_number = reduce(maximum_of_numbers, numbers)
+print(max_number)
+
+words = ["I","love", "python"]
+def concatenate_words(word_one, word_two):
+    return word_one + " " + word_two
+sentence = reduce(concatenate_words, words)
+print(sentence)
+
+numbers = [1,2,3,4,5]
+def square(number):
+    return number * number
+
+def product_of_squares(number_one, number_two):
+    return number_one * number_two
+squared_numbers = map(square, numbers)
+product = reduce(product_of_squares, squared_numbers)
+print(product)
+
+list_of_tuples = [(1,2), (3,4), (5,6)]
+
+def sum_of_tuples(tuple):
+    return tuple[0] + tuple[1]
+
+def is_sum_of_tuples_greater_than_5(tuple):
+    return sum_of_tuples(tuple) > 5
+
+result = filter(is_sum_of_tuples_greater_than_5, list_of_tuples)
+print(list(result))
+
+data = ["123", "456", "789", "abc", "def"]
+
+def sum_of_digits(data):
+    numeric_strings = filter(str.isdigit, data)
+    numbers = map(int, numeric_strings)
+    total = sum(numbers)
+    return total
+print(sum_of_digits(data))
