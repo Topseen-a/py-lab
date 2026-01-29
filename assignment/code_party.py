f780ea6195a6f90d2ce9b521519e1d16f95015c3
Topseen-a <abodunrintopseen@gmail.com>
2026-01-29 05:06:42 +0100

    code_party

--- assignment/code_party.py
+++ assignment/code_party.py
@@ -6,3 +6,22 @@ print(new_tuple)
 numbers = (1,2, [3,4], 5)
 numbers[2][1] = 99
 print(numbers)
+
+fruit_tuple = ("apple", "banana", "cherry")
+fruit_list = list(fruit_tuple)
+fruit_list.append("mango")
+fruit_tuple = tuple(fruit_list)
+print(fruit_tuple)
+
+tuple_number = (10, 20, 30, 40)
+a,b, *rest = tuple_number
+print(a)
+print(b)
+print(rest)
+
+input = [[2,3,4], [1,5,7], [4,6,8]]
+
+def sum_of_sublists(input):
+    return [sum(sublist) for sublist in input]
+
+print(sum_of_sublists(input))
