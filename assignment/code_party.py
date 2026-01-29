5006733bfb3bf14617f627ee964da4a0b7cfad2b
Topseen-a <abodunrintopseen@gmail.com>
2026-01-29 05:37:34 +0100

    code party

--- assignment/code_party.py
+++ assignment/code_party.py
@@ -20,8 +20,40 @@ print(b)
 print(rest)
 
 input = [[2,3,4], [1,5,7], [4,6,8]]
-
-def sum_of_sublists(input):
+def sum_of_index_sublists(input):
     return [sum(sublist) for sublist in input]
+print(sum_of_index_sublists(input))
 
+input = [[2,3,4], [1,5,7], [4,6,8]]
+def sum_of_sublists(input):
+    return [sum(group) for group in zip(*input)]
 print(sum_of_sublists(input))
+
+def is_even(number):
+    return number % 2 == 0
+even_numbers = filter(is_even, range(1, 21))
+print(list(even_numbers))
+
+words = ["cat", "elephant", "tiger", "lion"]
+def is_long_word(word):
+    return len(word) > 5
+long_words = filter(is_long_word, words)
+print(list(long_words))
+
+list_of_tuples = [(1, "A"), (4, "B"), (2, "C")]
+def first_element_greater_than_2(list_of_tuples):
+    return list_of_tuples[0] > 2
+printed_tuples = filter(first_element_greater_than_2, list_of_tuples)
+print(list(printed_tuples))
+
+def divisible_by_3_and_5(number):
+    return number % 3 == 0 and number % 5 == 0
+divisible_numbers = filter(divisible_by_3_and_5, range(1, 51))
+print(list(divisible_numbers))
+
+words = ["level", "world", "madam", "python"]
+def filter_palindromes(words):
+    return words == words[::-1]
+palindromes = filter(filter_palindromes, words)
+print(list(palindromes))
+
