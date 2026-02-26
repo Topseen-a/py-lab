4adc9c32b97b18e278c273f25708975d2197eb54
Topseen-a <abodunrintopseen@gmail.com>
2026-02-26 16:53:13 +0100

    semicolon

--- check_out_app/model/check_out.py
+++
@@ -1,3 +0,0 @@
-class Checkout:
-    VAT_RATE = 0.075
-
--- check_out_app/test/check_out_test.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from model.cart import Cart
-
-class CheckoutTest(unittest.TestCase):
-
-    def setUp(self):
-        self.cart = Cart()
