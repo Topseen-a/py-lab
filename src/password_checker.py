f4a4fed6b1967b57d385b6eb02a2467001f70fd8
Topseen-a <abodunrintopseen@gmail.com>
2026-06-08 20:40:24 +0100

    py

--- src/password_checker.py
+++ src/password_checker.py
@@ -1,19 +1,3 @@
-#Prompt user to "Enter your password:"
-#Read input of password
-
-#Calculate the length = number of characters in password
-
-#if length is greater than or equal to 6 AND less than or equal to 10 THEN
-    #strength = 'medium'
-#else if length is less than 6 THEN
-    #strength = 'weak'
-#else if length is greater than 10 THEN
-    #strength = 'strong'
-#else
-    #strength = 'invalid'
-
-#Display 'Password strength:', strength
-
 password = input('Enter your password: ')
 
 length = len(password)
